92f70e2a61041c05f8a44291a17f1bedcc03a618
alexisdlf <51085627+alexisdlf@users.noreply.github.com>
2019-05-27 15:22:59 +0200

    Create streamError.py

---
+++ streamError.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  6 13:06:26 2018
+
+@author: Alexis
+"""
+
+class UnConnctedStreamError(Exception):
+    def __init__(self, action):
+        self.action = action
+    def __str__(self):
+        return "tentative de " + self.action + " sur un StreamListener non connecté"
+    
+class RecordingSreamError(Exception):
+    def __init__(self):
+        pass
+    def __str__(self):
+        return "retrait du buffer sur un stream en train d' enregistrer"
