378e41808ac2cd8286a532d07bfa61a6865ed10f
alexisdlf <51085627+alexisdlf@users.noreply.github.com>
2019-05-27 15:22:08 +0200

    Create arduino.py

---
+++ arduino.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 18:42:59 2019
+
+@author: Alexis
+"""
+
+from serial import Serial
+
+class Arduino:
+    def __init__(self, port):
+        self.usb = Serial(port=port, baudrate='9600')
+        
+    def sendInt(self, i):
+        self.usb.write(bytes([i]))
+        
+        
